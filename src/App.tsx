65a2778ce922dd02dd06b3e972fef6a1bf30ecf0
TSecretT <timichfull@gmail.com>
2021-05-26 16:13:39 +0600

    Init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import { Home } from './pages';
+
+const App = () => {
+
+
+	return (
+		<Router>
+			<Switch>
+			<Route path="/" />
+			</Switch>
+		</Router>
+	)
+}
+
+export default App;
